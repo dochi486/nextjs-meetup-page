8e4c39010b6c43963a821f9379461a344c53e202
dochi486 <93mskim@gmail.com>
2022-10-04 13:05:24 +0900

    first commit

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: '테스트 밋업',
+    image:
+      'https://lh3.googleusercontent.com/NHeXUhVPpVhctn2gBZyO8Z1U4Dbm9ahXxPT4Oin6UgjjmS8lhnudjELu-s8ktthJ8HE962wcJ6I-3oWtqZBoTvf_hHI=w2048',
+    address: '어딘가, 어디어디',
+    description: '첫번째 밋업입니당',
+  },
+  {
+    id: 'm2',
+    title: '두번째 밋업',
+    image:
+      'https://lh3.googleusercontent.com/NHeXUhVPpVhctn2gBZyO8Z1U4Dbm9ahXxPT4Oin6UgjjmS8lhnudjELu-s8ktthJ8HE962wcJ6I-3oWtqZBoTvf_hHI=w2048',
+    address: '어딘가, 어디어디',
+    description: '두번째 밋업입니당',
+  },
+];
+
+function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+}
+
+export default HomePage;

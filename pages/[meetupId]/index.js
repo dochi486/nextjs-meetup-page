67ee3417c080881c59d481412f6ed99c339eec10
dochi486 <93mskim@gmail.com>
2022-10-04 13:28:34 +0900

    밋업 디테일 컴포넌트 분리

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://lh3.googleusercontent.com/NHeXUhVPpVhctn2gBZyO8Z1U4Dbm9ahXxPT4Oin6UgjjmS8lhnudjELu-s8ktthJ8HE962wcJ6I-3oWtqZBoTvf_hHI=w2048"
+      title="첫 테스트 밋업"
+      address="어디,어디어디"
+      description="첫번째 테스트 밋업입니다."
+    />
+  );
+}
+
+export default MeetupDetails;
